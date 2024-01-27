a64185b25a6aa019967ceae897ba61bc789cad8b
Hazem Mathbout <hazemmathbout93@gmail.com>
2024-01-27 06:49:16 +0200

    ccc commit

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,12 @@ RUN apt-get update && \
     apt-get install -y python3-pip && \
     pip install --no-cache-dir -r requirements.txt
 COPY . .
+# Create a directory to store the chromedriver
+RUN mkdir /app/drivers
+# Copy chromedriver to the desired location in the container
+COPY drivers/chromedriver /app/drivers/
+# Set execute permissions on chromedriver
+RUN chmod +x /app/drivers/chromedriver
+
 # CMD uvicorn main:app --host 0.0.0.0 --port $PORT
 CMD ["gunicorn", "app:app", "--bind", "0.0.0.0:80"]
--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def scrape_and_display(product_url):
     # print("List files in the 'drivers' directory:", files_in_drivers)
     # chrome_options.binary_location = os.environ.get("/usr/bin/google-chrome")
     # chrome_driver_path = ChromeDriverManager().install()
-    chrome_driver_path = os.path.abspath("./drivers/chromedriver")
+    chrome_driver_path = "/app/drivers/chromedriver"
     service = webdriver.ChromeService(executable_path= chrome_driver_path)
     
     # service = webdriver.ChromeService(executable_path= ChromeDriverManager().install())
