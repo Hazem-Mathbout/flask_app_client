897a569485c4fd86bb5f0bb7fb04f580f60d909f
Hazem Mathbout <hazemmathbout93@gmail.com>
2024-01-27 02:56:21 +0200

    tenth commit

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, request
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+
 from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
 import os 
 import time
 
@@ -26,15 +28,29 @@ def extract_numerical_value(text):
         return 0
 def scrape_and_display(product_url):
     chrome_options = webdriver.ChromeOptions()
-    print("Current directory:", os.getcwd())
-    print("List files in the current directory:", os.listdir())
-    drivers_directory = "./drivers"
-    files_in_drivers = os.listdir(drivers_directory)
-    print("List files in the 'drivers' directory:", files_in_drivers)
+    # print("Current directory:", os.getcwd())
+    # print("List files in the current directory:", os.listdir())
+    # drivers_directory = "./drivers"
+    # files_in_drivers = os.listdir(drivers_directory)
+    # print("List files in the 'drivers' directory:", files_in_drivers)
     # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     # chrome_driver_path = ChromeDriverManager().install()
-    chrome_driver_path = os.path.abspath("./drivers/chromedriver")
-    service = webdriver.ChromeService(executable_path=chrome_driver_path)
+    # chrome_driver_path = os.path.abspath("./drivers/chromedriver")
+    # service = webdriver.ChromeService(executable_path= ChromeDriverManager().install())
+    
+    service = webdriver.ChromeService(executable_path= os.environ.get("CHROMEDRIVER_PATH"))
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--proxy-server='direct://'")
+    chrome_options.add_argument("--proxy-bypass-list=*")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--allow-running-insecure-content')
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument('--headless')
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.28 Safari/537.36'
     chrome_options.add_argument(f'user-agent={user_agent}')
@@ -59,6 +75,7 @@ def scrape_and_display(product_url):
             result["result_text"] = sentence
         page_source = driver.page_source
         soup = BeautifulSoup(page_source, 'html.parser')
+        
         product_price_element = soup.find('div', class_='es--wrap--erdmPRe')
         product_price_text = product_price_element.text.strip().replace('€', '') if product_price_element else 'Not found'
         shipping_price_element = soup.find('div', class_='dynamic-shipping-titleLayout')
@@ -156,3 +173,151 @@ def index():
     return render_template('index.html', result=None)
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+# from flask import Flask, render_template, request
+# import requests
+# from bs4 import BeautifulSoup
+# import os 
+# import time
+
+# app = Flask(__name__)
+
+# def extract_numerical_value(text):
+#     try:
+#         return float(''.join(c for c in text if c.isdigit() or c in ['.', ',']))
+#     except ValueError:
+#         return 0
+
+# def scrape_and_display(product_url):
+#     result = {
+#         "result_text": "",
+#         "price": 0,
+#         "shipping": 0,
+#         "total": 0,
+#         "get_day": 0  
+#     }
+#     try:
+#         # headers = {
+#         #     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36'
+#         # }
+#         response = requests.get(product_url)
+#         response.raise_for_status()
+
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#         print("===============================")
+#         print("Soup is: ")
+#         print(soup)
+#         print("===============================")
+#         product_price_element = soup.find('div', class_='es--wrap--erdmPRe notranslate')
+#         print("==================")
+#         print(product_price_element)
+#         print("==================")
+#         product_price_text = product_price_element.text.strip().replace('€', '') if product_price_element else 'Not found'
+        
+#         shipping_price_element = soup.find('div', class_='dynamic-shipping-titleLayout')
+#         shipping_price_text = shipping_price_element.text.strip().replace('€', '') if shipping_price_element else 'Not found'
+#         relevant_shipping_info = shipping_price_text.split(':')[-1].strip()
+        
+#         product_price = extract_numerical_value(product_price_text)
+#         shipping_price = extract_numerical_value(relevant_shipping_info)
+#         total_value = product_price + shipping_price
+#         print(f"total_value : {total_value}")
+#         # The rest of your logic for calculating 'get_day' goes here...
+
+#         if total_value > 0:
+#             if total_value < 4:
+#                 result['get_day'] = total_value * 239 + 300
+#             elif total_value < 6:
+#                 result['get_day'] = total_value * 239 + 350
+#             elif total_value < 7:
+#                 result['get_day'] = total_value * 239 + 400
+#             elif total_value < 8:
+#                result['get_day'] = total_value * 239 + 450
+#             elif total_value < 12:
+#                 result['get_day'] = total_value * 239 + 500
+#             elif total_value < 16:
+#                 result['get_day'] = total_value * 239 + 700
+#             elif total_value < 20:
+#                  result['get_day'] = total_value * 239 + 800
+#             elif total_value < 24:
+#                result['get_day'] = total_value * 239 + 900
+#             elif total_value < 28:
+#                result['get_day'] = total_value * 239 + 1000
+#             elif total_value < 35:
+#                result['get_day'] = total_value * 239 + 1100
+#             elif total_value < 40:
+#                result['get_day'] = total_value * 239 + 1200
+#             elif total_value < 50:
+#                result['get_day'] = total_value * 239 + 1300
+#             elif total_value < 60:
+#                result['get_day'] = total_value * 239 + 1400
+#             elif total_value < 70:
+#                  result['get_day'] = total_value * 239 + 1500
+#             elif total_value < 80:
+#                 result['get_day'] = total_value * 239 + 1600
+#             elif total_value < 90:
+#                result['get_day'] = total_value * 239 + 1700
+#             elif total_value < 100:
+#                 result['get_day'] = total_value * 239 + 1800
+#             elif total_value < 110:
+#                result['get_day'] = total_value * 239 + 1900
+#             elif total_value < 120:
+#                result['get_day'] = total_value * 239 + 2000
+#             elif total_value < 130:
+#                result['get_day'] = total_value * 239 + 2100
+#             elif total_value < 140:
+#                result['get_day'] = total_value * 239 + 2200
+#             elif total_value < 150:
+#                result['get_day'] = total_value * 239 + 2300
+#             elif total_value < 160:
+#                result['get_day'] = total_value * 239 + 2400
+#             elif total_value < 170:
+#                result['get_day'] = total_value * 239 + 2500
+#             elif total_value < 180:
+#                result['get_day'] = total_value * 239 + 2600
+#             elif total_value < 190:
+#                 result['get_day'] = total_value * 239 + 2700
+#             elif total_value < 200:
+#                result['get_day'] = total_value * 239 + 2800
+#             elif total_value < 230:
+#                 result['get_day'] = total_value * 239 + 2900
+#             elif total_value < 260:
+#                 result['get_day'] = total_value * 239 + 3000
+#             elif total_value < 280:
+#                result['get_day'] = total_value * 239 + 3100
+#             elif total_value < 300:
+#                result['get_day'] = total_value * 239 + 3400
+#             elif total_value >= 300:
+#                 z = total_value / 100
+#                 result['get_day'] = total_value * 239 + z * 1600
+#             mod = result['get_day'] % 50
+#             if mod != 0:
+#                 result['get_day'] = result['get_day'] + 50 - mod
+
+#     except Exception as e:
+#         print(f"Error during scraping: {e}")
+#         result["error"] = str(e)
+
+#     result.update({
+#         "price": product_price,
+#         "shipping": shipping_price,
+#         "total": total_value,
+#     })
+
+#     return result
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         product_url = request.form['product_url']
+#         result = scrape_and_display(product_url)
+#         return render_template('index.html', result=result)
+#     return render_template('index.html', result=None)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
