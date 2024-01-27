02f8cba53c61eecd3d953046ef099f831ef8050f
Hazem Mathbout <hazemmathbout93@gmail.com>
2024-01-27 09:27:10 +0200

    BBB commit

--- app.py
+++ app.py
@@ -55,9 +55,9 @@ def scrape_and_display(product_url):
     # chrome_options.add_argument("--proxy-server='direct://'")
     # chrome_options.add_argument("--proxy-bypass-list=*")
     # chrome_options.add_argument("--start-maximized")
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     # chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--no-sandbox')
     # chrome_options.add_argument('--ignore-certificate-errors')
     # chrome_options.add_argument('--allow-running-insecure-content')
     # chrome_options.add_argument("--disable-dev-shm-usage")
@@ -183,7 +183,7 @@ def index():
         return render_template('index.html', result=result)
     return render_template('index.html', result=None)
 if __name__ == '__main__':
-    app.run(debug=False,host= '0.0.0.0', threaded=True)
+    app.run(debug=False, threaded=True)
 
 
 
