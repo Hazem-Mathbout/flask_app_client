7c0a5a2269bd05403cfc51c86a5341bdc2e22bf5
Hazem Mathbout <hazemmathbout93@gmail.com>
2024-01-27 08:45:10 +0200

    GGG commit

--- app.py
+++ app.py
@@ -58,13 +58,13 @@ def scrape_and_display(product_url):
     chrome_options.add_argument('--headless')
     # chrome_options.add_argument('--disable-gpu')
     # chrome_options.add_argument('--disable-dev-shm-usage')
-    # chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--no-sandbox')
     # chrome_options.add_argument('--ignore-certificate-errors')
-    # chrome_options.add_argument('--allow-running-insecure-content')
+    chrome_options.add_argument('--allow-running-insecure-content')
     # chrome_options.add_argument("--disable-dev-shm-usage")
     # chrome_options.add_argument('--headless')
-    # user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.28 Safari/537.36'
-    # chrome_options.add_argument(f'user-agent={user_agent}')
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6167.85 Safari/537.36'
+    chrome_options.add_argument(f'user-agent={user_agent}')
     driver = webdriver.Chrome(service=service, options=chrome_options)
     result = {
         "result_text": "",
